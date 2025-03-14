9f556e7e34cd73ea64a9a1511a67d2cdf84cf9d4
Bowhle <bowhlem@gmail.com>
2025-03-14 16:48:12 +0200

    frontend working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
 import Homepage from './components/Homepage';
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 import Header from './components/Header';
+
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <main className="main-content">
+//         <Homepage />
+//       </main>
+// <Footer />
+//     </div>
+//   );
+// }
 
 function App() {
   return (
     <div>
       <Header />
       <main className="main-content">
-        <Homepage />
-      </main>
-      <Footer />
+         <Homepage />
+       </main>
     </div>
   );
 }
-
 export default App;
